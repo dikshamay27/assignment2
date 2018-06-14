50e85a33b61059f8ec45e12b68debf501b79ea69
dikshamay27 <may27diksha@gmail.com>
2018-06-14 14:43:56 +0530

    file added

---
+++ aasignment2.py
@@ -0,0 +1,36 @@
+#Question 1 : print anything you want to put on screen
+print("My name id Diksha khanna")
+
+
+#Question 2 : Join two strings using '+'.
+
+
+
+print("diksha+meenaakshi")
+
+#Question 3 : Take the input of 3 variables x,y and z. print their values on screen.
+
+
+
+x=int(input("enter the value of x"))
+y=int(input("enter the value of y"))
+z=(x+y)
+print(z)
+print(type(z))
+
+#Question 4 : print "let's get started" on screen.
+
+
+print("let's get started")
+
+
+#Question 5 : let's do some intresting exercise:
+#name="Tony stark"
+#salary=1000000
+#print('%s''%d')%(_____,______)
+
+
+
+name=("tonny strak")
+salary=int(1000000
+print("%s,%d%"(name,salary))
